1a2d8bcd6123cd76dec6eeca37297c4688a59d87
Soumya Singh <singhsoumya0804@gmail.com>
2026-04-20 18:47:22 -0700

    Changed from communities to events

---
+++ my-react-app/src/pages/EventsPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function EventsPage() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1 style={{ marginTop: 0 }}>Events</h1>
+      <p>Follow the latest buy/sell events here.</p>
+    </main>
+  );
+}
